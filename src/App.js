050556efa4924d946ab7759d462ee9cb076264da
Rahul Netkar <rsn9.rn@gmail.com>
2022-02-09 11:58:38 +0530

    Header component set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { CssBaseline, Grid } from '@material-ui/core'
 //component import
 import Header from './components/Header/Header';
 import List from './components/List/List';
@@ -7,9 +8,18 @@ import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }} >
+        <Grid item xs={12} md={4} >
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8} >
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Map = () => {
     return (
-        <div>Map</div>
+        <h1>Map</h1>
     );
 };
 
